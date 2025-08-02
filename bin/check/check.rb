05ed07358b65a2636d74e22736c51dbb4feeb3a4
Willem van der Schyff <willemvds@gmail.com>
2025-08-02 20:08:58 +0200

    feat: Move bin/check.rb from single fqdn to lst file.

--- bin/check/check.rb
+++ bin/check/check.rb
@@ -18,25 +18,30 @@ EXIT_CODE_FAILURE = 2
 args = ARGV
 
 if args.length != 1
-  puts 'Usage: check <fqdn>'
+  puts 'Usage: check <domain.lst>'
   exit(EXIT_CODE_USAGE)
 end
 
-fqdn = args[0]
-
-begin
-  event = Check.domain fqdn
-rescue Check::Bad => e
-  puts "check domain err=#{e.message}"
-  exit(EXIT_CODE_FAILURE)
-end
-
-event_json = JSON.generate(event)
-puts event_json
+domains_path = args[0]
+domains = File.read(domains_path).split
+domains = domains.select { |domain| domain.length > 0 }
 
 connection = Bunny.new
 connection.start
 channel = connection.create_channel
 queue_name = 'https'
-queue = channel.queue(queue_name, durable: true, arguments: { 'x-queue-type' => 'quorum' })
-puts channel.default_exchange.publish(event_json, routing_key: queue.name)
+
+domains.each do |fqdn|
+  begin
+    event = Check.domain fqdn
+  rescue Check::Bad => e
+    puts "check domain err=#{e.message}"
+    exit(EXIT_CODE_FAILURE)
+  end
+
+  event_json = JSON.generate(event)
+  puts event_json
+
+  queue = channel.queue(queue_name, durable: true, arguments: { 'x-queue-type' => 'quorum' })
+  puts channel.default_exchange.publish(event_json, routing_key: queue.name)
+end
