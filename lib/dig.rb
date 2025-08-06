e6c6558895df02176973c88828ac91df59770889
Willem van der Schyff <willemvds@gmail.com>
2025-08-06 06:59:15 +0200

    feat: Add module to wrap dig binary.

---
+++ lib/dig.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'open3'
+
+module Dig
+  DEFAULT_BINARY_PATH = 'dig'
+  private_constant :DEFAULT_BINARY_PATH
+
+  class Error < StandardError
+  end
+
+  class BinaryUnavailable < Error
+  end
+
+  class NoResults < Error
+  end
+
+  def self.ips(fqdn, binary_path = DEFAULT_BINARY_PATH)
+    begin
+      stdout, stderr, status = Open3.capture3(binary_path, fqdn, '+short')
+    rescue SystemCallError => e
+      raise BinaryUnavailable, e
+    end
+
+    raise Error, "Unexpected dig error: exit code=#{status.exitstatus}, stderr=#{stderr}" unless status.success?
+
+    ips = stdout.split
+
+    raise NoResults if ips.length == 0
+
+    ips
+  end
+end
