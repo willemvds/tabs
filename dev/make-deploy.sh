38e36ba8a559fa3ff8af0354e97c9d2e1d6c4ace
Willem van der Schyff <willemvds@gmail.com>
2025-08-09 16:58:49 +0200

    feat: Add some usage help to make-deploy.sh.

--- dev/make-deploy.sh
+++ dev/make-deploy.sh
@@ -3,6 +3,15 @@
 VERSION=$1
 INCLUDE_VENDOR=$2
 
+if [[ -z "$VERSION" ]]; then
+    printf "Usage: make-deploy.sh <semantic-version> <include-vendor>\n"
+    printf "Example:\n"
+    printf "	make-deploy v1.0.0\n"
+    printf "	make-deploy v1.0.1 vendor\n"
+    printf "\n"
+    exit 2
+fi
+
 if [ -n "$INCLUDE_VENDOR" ]; then
     INCLUDE_VENDOR="vendor/ruby/3.4.0"
 fi
