6b279dca471ea633c5f2e1b6fecd75a4ae677060
Willem van der Schyff <willemvds@gmail.com>
2025-08-09 16:52:53 +0200

    feat: Poor man's "with-vendor" for make-deploy.

--- dev/make-deploy.sh
+++ dev/make-deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 VERSION=$1
+INCLUDE_VENDOR=$2
 
-# Excluding .ruby-version till we can figure out debian vs arch situation.
-tar cvz -f tabs-$1.tar.xz bin/ lib/ ui/ autoloader.rb Gemfile Gemfile.lock LICENSE README.md
+if [ -n "$INCLUDE_VENDOR" ]; then
+    INCLUDE_VENDOR="vendor/ruby/3.4.0"
+fi
+
+tar cvz -f tabs-$1.tar.xz bin/ lib/ ui/ $INCLUDE_VENDOR .ruby-version autoloader.rb Gemfile Gemfile.lock LICENSE README.md
 
